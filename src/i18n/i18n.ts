216927ac33d4894eb2ad32ad0d0f51f86914a7dd
MiroslavPopovich <miroslav.popovich.89@gmail.com>
2024-06-05 00:09:50 +0100

    fixed defaultLanguage issue, set lit-html and page dependencies

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,104 +1,118 @@
 const availableLocales: string[] = ["en", "bg"];
-const defaultLanguage: string = navigator.language.split("-")[0];
-
-const fetchLocale = async (locale: string): Promise<{ [key: string]: any }> => {
-  const response = await fetch(`../../src/locales/${locale}.json`);
-  return await response.json();
-};
-
+const defaultLanguage: string = "en";
 const locales: { [key: string]: { [key: string]: any } } = {};
 
-const loadLocales = async () => {
-  for (const locale of availableLocales) {
-    locales[locale] = await fetchLocale(locale);
-  }
+const fetchLocale = async (locale: string): Promise<{ [key: string]: any }> => {
+    try {
+        const response: { [key: string]: any } = await fetch(
+            `../../src/locales/${locale}.json`
+        );
+        const responseContentType: any = response["headers"]
+            .get("Content-Type")
+            .split(";")[0];
+        if (responseContentType !== "application/json") {
+            throw new Error(`Error! Status: ${locale}.json doesn't exist`);
+        }
+        return await response.json();
+    } catch (error) {
+        // ToDo
+        throw error;
+    }
+};
+
+const loadLocales = async (): Promise<void> => {
+    for (const locale of availableLocales) {
+        locales[locale] = await fetchLocale(locale);
+    }
 };
 
 const detectLanguage = (): string => {
-  let language: string = (
-    navigator.language || (navigator as any).userLanguage
-  ).substr(0, 2);
+    let language: string = (
+        navigator.language || (navigator as any).userLanguage
+    ).substr(0, 2);
 
-  const urlParams: URLSearchParams = new URLSearchParams(
-    window.location.search,
-  );
-  const langFromUrl: string | null = urlParams.get("lang");
-  if (langFromUrl && availableLocales.indexOf(langFromUrl) !== -1) {
-    language = langFromUrl;
-  }
+    const urlParams: URLSearchParams = new URLSearchParams(
+        window.location.search
+    );
+    const langFromUrl: string | null = urlParams.get("lang");
+    if (langFromUrl && availableLocales.indexOf(langFromUrl) !== -1) {
+        language = langFromUrl;
+    }
 
-  return availableLocales.indexOf(language) !== -1 ? language : defaultLanguage;
+    return availableLocales.indexOf(language) !== -1
+        ? language
+        : defaultLanguage;
 };
 
 const getNestedProperty = (obj: any, key: string): string | null => {
-  return key
-    .split(".")
-    .reduce((o, k) => (o && o[k] !== "undefined" ? o[k] : null), obj);
+    return key
+        .split(".")
+        .reduce((o, k) => (o && o[k] !== "undefined" ? o[k] : null), obj);
 };
 
 const updatePageLanguage = (lang: string): void => {
-  const pageLanguage: string =
-    availableLocales.indexOf(lang) !== -1 ? lang : defaultLanguage;
-  const elements: NodeListOf<HTMLElement> =
-    document.querySelectorAll("[data-i18n]");
+    const pageLanguage: string = lang;
+    const elements: NodeListOf<HTMLElement> =
+        document.querySelectorAll("[data-i18n]");
 
-  try {
-    const json: { [key: string]: any } = locales[pageLanguage];
+    try {
+        const json: { [key: string]: any } = locales[pageLanguage];
 
-    elements.forEach((element) => {
-      const key: string | null = element.getAttribute("data-i18n");
-      if (!key) return;
+        elements.forEach((element) => {
+            const key: string | null = element.getAttribute("data-i18n");
+            if (!key) return;
 
-      let text: string | null = getNestedProperty(json, key);
-      if (!text) return;
+            let text: string | null = getNestedProperty(json, key);
+            if (!text) return;
 
-      const variables: RegExpMatchArray | null = text.match(/{(.*?)}/g);
-      if (variables) {
-        variables.forEach((variable) => {
-          const datasetEntries: [string, string][] = Object.keys(
-            element.dataset,
-          ).map((key) => [key, element.dataset[key]!]);
-          datasetEntries.forEach(([dataKey, value]) => {
-            if (`{${dataKey}}` === variable) {
-              try {
-                text = text!.replace(
-                  `${variable}`,
-                  new Function(`return (${value})`)(),
-                );
-              } catch (error) {
-                text = text!.replace(`${variable}`, value);
-              }
+            const variables: RegExpMatchArray | null = text.match(/{(.*?)}/g);
+            if (variables) {
+                variables.forEach((variable) => {
+                    const datasetEntries: [string, string][] = Object.keys(
+                        element.dataset
+                    ).map((key) => [key, element.dataset[key]!]);
+                    datasetEntries.forEach(([dataKey, value]) => {
+                        if (`{${dataKey}}` === variable) {
+                            try {
+                                text = text!.replace(
+                                    `${variable}`,
+                                    new Function(`return (${value})`)()
+                                );
+                            } catch (error) {
+                                text = text!.replace(`${variable}`, value);
+                            }
+                        }
+                    });
+                });
             }
-          });
+
+            element.innerHTML = text;
         });
-      }
 
-      element.innerHTML = text;
-    });
-
-    const htmlElement: HTMLElement | null = document.querySelector("html");
-    if (htmlElement) {
-      htmlElement.setAttribute("lang", pageLanguage);
+        const htmlElement: HTMLElement | null = document.querySelector("html");
+        if (htmlElement) {
+            htmlElement.setAttribute("lang", pageLanguage);
+        }
+    } catch (error) {
+        console.error(`Error updating page language to ${lang}:`, error);
     }
-  } catch (error) {
-    console.error(`Error updating page language to ${lang}:`, error);
-  }
 };
 
-const init = async () => {
-  await loadLocales();
-  const language: string = detectLanguage();
-  updatePageLanguage(language);
+const init = async (): Promise<void> => {
+    await loadLocales();
+    const language: string = detectLanguage();
+    updatePageLanguage(language);
 
-  document.querySelectorAll(".change-language").forEach((button) => {
-    button.addEventListener("click", (event: Event) => {
-      const target = event.target as HTMLElement;
-      const newLanguage: string | null = target.getAttribute("data-language");
-      if (newLanguage && availableLocales.indexOf(newLanguage) !== -1) {
-        updatePageLanguage(newLanguage);
-      }
+    document.querySelectorAll(".change-language").forEach((button) => {
+        button.addEventListener("click", (event: Event) => {
+            const target = event.target as HTMLElement;
+            const newLanguage: string | null =
+                target.getAttribute("data-language");
+            if (newLanguage && availableLocales.indexOf(newLanguage) !== -1) {
+                updatePageLanguage(newLanguage);
+            }
+        });
     });
-  });
 };
 
 // Initialize the application
